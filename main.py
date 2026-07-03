d0ae48110cfc6301a70033370ba0fc543a48cab9
vivi <zunigaal2003@gmail.com>
2026-07-03 09:11:21 -0600

    Add POST endpoint to create todos

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ class TodoRequest(BaseModel):
     description: str = Field(min_length=3, max_length=100)
     priority: int = Field(gt=0, lt=6)
     complete: bool
-    
+
 
 @app.get("/")
 async def read_all(db: db_dependency):
@@ -40,3 +40,12 @@ async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
     if todo_model is not None:
         return todo_model
     raise HTTPException(status_code=404, detail="Todo not found")
+
+
+@app.post("/todo/", status_code=status.HTTP_201_CREATED)
+async def create_todo(db: db_dependency, todo_request: TodoRequest):
+    todo_model = Todos(**todo_request.dict())
+    db.add(todo_model)
+    db.commit()
+    db.refresh(todo_model)
+    return todo_model
