c227c230656ad95c61b9cd456a94638526e6e1dd
vivi <zunigaal2003@gmail.com>
2026-07-07 09:50:15 -0600

    Restore the Users model, fix the bcrypt bug, and configure db_dependency

--- routers/auth.py
+++ routers/auth.py
@@ -1,8 +1,12 @@
 from fastapi import APIRouter
-from pydantic import BaseModel, Field
+from pydantic import BaseModel
 from models import Users
+from passlib.context import CryptContext
+from .todos import db_dependency
 
 router = APIRouter()
+
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 class CreateUserRequest(BaseModel):
     username: str
@@ -13,15 +17,18 @@ class CreateUserRequest(BaseModel):
     role: str
 
 @router.post("/auth")
-async def create_user(create_user_request: CreateUserRequest):
+async def create_user(create_user_request: CreateUserRequest, db: db_dependency):
     create_user_model = Users(
         username=create_user_request.username,
         email=create_user_request.email,
         first_name=create_user_request.first_name,
         last_name=create_user_request.last_name,
-        hashed_password=create_user_request.password,
+        hashed_password=bcrypt_context.hash(create_user_request.password),
         role=create_user_request.role,
         is_active=True
     )
+    db.add(create_user_model)  
+    db.commit()               
+    db.refresh(create_user_model)
 
-    return {"user": create_user_model}
+    return create_user_model
